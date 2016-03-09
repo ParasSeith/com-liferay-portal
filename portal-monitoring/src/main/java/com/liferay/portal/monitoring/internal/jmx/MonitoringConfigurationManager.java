brianchandotcom/liferay-portal

LPS-64056 Apply everywhere else

modules/apps/foundation/portal/portal-monitoring/src/main/java/com/liferay/portal/monitoring/internal/jmx/MonitoringConfigurationManager.java
@@ -14,8 +14,7 @@
 
 package com.liferay.portal.monitoring.internal.jmx;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
+import com.liferay.portal.configuration.ConfigurableUtil;
 import com.liferay.portal.kernel.monitoring.Level;
 import com.liferay.portal.kernel.monitoring.MethodSignature;
 import com.liferay.portal.kernel.monitoring.MonitoringControl;
@@ -198,7 +197,7 @@ public class MonitoringConfigurationManager
 	@Activate
 	@Modified
 	protected void activate(Map<String, Object> properties) {
-		_monitoringConfiguration = Configurable.createConfigurable(
+		_monitoringConfiguration = ConfigurableUtil.createConfigurable(
 			MonitoringConfiguration.class, properties);
 
 		setMonitorPortalRequest(

modules/apps/foundation/portal/portal-monitoring/src/main/java/com/liferay/portal/monitoring/internal/messaging/MonitoringMessagingConfigurator.java
@@ -14,8 +14,7 @@
 
 package com.liferay.portal.monitoring.internal.messaging;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
+import com.liferay.portal.configuration.ConfigurableUtil;
 import com.liferay.portal.kernel.concurrent.DiscardOldestPolicy;
 import com.liferay.portal.kernel.concurrent.RejectedExecutionHandler;
 import com.liferay.portal.kernel.concurrent.ThreadPoolExecutor;
@@ -53,7 +52,7 @@ public class MonitoringMessagingConfigurator {
 			componentContext.getProperties();
 
 		MonitoringConfiguration monitoringConfiguration =
-			Configurable.createConfigurable(
+			ConfigurableUtil.createConfigurable(
 				MonitoringConfiguration.class, properties);
 
 		DestinationConfiguration destinationConfiguration =

modules/apps/foundation/portal/portal-monitoring/src/main/java/com/liferay/portal/monitoring/internal/servlet/taglib/MonitoringBottomDynamicInclude.java
@@ -14,8 +14,7 @@
 
 package com.liferay.portal.monitoring.internal.servlet.taglib;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
+import com.liferay.portal.configuration.ConfigurableUtil;
 import com.liferay.portal.kernel.monitoring.DataSample;
 import com.liferay.portal.kernel.monitoring.DataSampleThreadLocal;
 import com.liferay.portal.kernel.monitoring.RequestStatus;
@@ -103,7 +102,7 @@ public class MonitoringBottomDynamicInclude extends BaseDynamicInclude {
 	@Activate
 	@Modified
 	protected void activate(Map<String, Object> properties) {
-		_monitoringConfiguration = Configurable.createConfigurable(
+		_monitoringConfiguration = ConfigurableUtil.createConfigurable(
 			MonitoringConfiguration.class, properties);
 	}
 

modules/apps/foundation/portal/portal-monitoring/src/main/java/com/liferay/portal/monitoring/internal/servlet/taglib/MonitoringTopHeadDynamicInclude.java
@@ -14,8 +14,7 @@
 
 package com.liferay.portal.monitoring.internal.servlet.taglib;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
+import com.liferay.portal.configuration.ConfigurableUtil;
 import com.liferay.portal.kernel.monitoring.DataSample;
 import com.liferay.portal.kernel.monitoring.DataSampleFactory;
 import com.liferay.portal.kernel.monitoring.DataSampleThreadLocal;
@@ -83,7 +82,7 @@ public class MonitoringTopHeadDynamicInclude extends BaseDynamicInclude {
 	@Activate
 	@Modified
 	protected void activate(Map<String, Object> properties) {
-		_monitoringConfiguration = Configurable.createConfigurable(
+		_monitoringConfiguration = ConfigurableUtil.createConfigurable(
 			MonitoringConfiguration.class, properties);
 	}
 

modules/apps/foundation/portal/portal-upgrade/src/main/java/com/liferay/portal/upgrade/internal/release/ReleaseManager.java
@@ -14,13 +14,12 @@
 
 package com.liferay.portal.upgrade.internal.release;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
 import com.liferay.osgi.service.tracker.collections.map.PropertyServiceReferenceComparator;
 import com.liferay.osgi.service.tracker.collections.map.PropertyServiceReferenceMapper;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapListener;
+import com.liferay.portal.configuration.ConfigurableUtil;
 import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.dao.db.DBContext;
 import com.liferay.portal.kernel.dao.db.DBManagerUtil;
@@ -124,7 +123,7 @@ public class ReleaseManager {
 		ServiceTrackerMapListener<String, UpgradeInfo, List<UpgradeInfo>>
 			serviceTrackerMapListener = null;
 
-		_releaseManagerConfiguration = Configurable.createConfigurable(
+		_releaseManagerConfiguration = ConfigurableUtil.createConfigurable(
 			ReleaseManagerConfiguration.class, properties);
 
 		if (_releaseManagerConfiguration.autoUpgrade()) {

modules/apps/foundation/portal/portal-verify-extender/src/main/java/com/liferay/portal/verify/extender/internal/VerifyProcessTracker.java
@@ -14,14 +14,13 @@
 
 package com.liferay.portal.verify.extender.internal;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
 import com.liferay.counter.kernel.service.CounterLocalService;
 import com.liferay.osgi.service.tracker.collections.map.PropertyServiceReferenceComparator;
 import com.liferay.osgi.service.tracker.collections.map.PropertyServiceReferenceMapper;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapListener;
+import com.liferay.portal.configuration.ConfigurableUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Release;
@@ -132,8 +131,9 @@ public class VerifyProcessTracker {
 	protected void activate(
 		BundleContext bundleContext, Map<String, Object> properties) {
 
-		_verifyProcessTrackerConfiguration = Configurable.createConfigurable(
-			VerifyProcessTrackerConfiguration.class, properties);
+		_verifyProcessTrackerConfiguration =
+			ConfigurableUtil.createConfigurable(
+				VerifyProcessTrackerConfiguration.class, properties);
 
 		ServiceTrackerMapListener<String, VerifyProcess, List<VerifyProcess>>
 			verifyServiceTrackerMapListener = null;

ParasSeith/com-liferay-portal

portal-monitoring/src/main/java/com/liferay/portal/monitoring/internal/jmx/MonitoringConfigurationManager.java
@@ -14,8 +14,7 @@
 
 package com.liferay.portal.monitoring.internal.jmx;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
+import com.liferay.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.monitoring.Level;
 import com.liferay.portal.kernel.monitoring.MethodSignature;
 import com.liferay.portal.kernel.monitoring.MonitoringControl;
@@ -198,7 +197,7 @@ public class MonitoringConfigurationManager
 	@Activate
 	@Modified
 	protected void activate(Map<String, Object> properties) {
-		_monitoringConfiguration = Configurable.createConfigurable(
+		_monitoringConfiguration = ConfigurableUtil.createConfigurable(
 			MonitoringConfiguration.class, properties);
 
 		setMonitorPortalRequest(

portal-monitoring/src/main/java/com/liferay/portal/monitoring/internal/messaging/MonitoringMessagingConfigurator.java
@@ -14,8 +14,7 @@
 
 package com.liferay.portal.monitoring.internal.messaging;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
+import com.liferay.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.concurrent.DiscardOldestPolicy;
 import com.liferay.portal.kernel.concurrent.RejectedExecutionHandler;
 import com.liferay.portal.kernel.concurrent.ThreadPoolExecutor;
@@ -53,7 +52,7 @@ public class MonitoringMessagingConfigurator {
 			componentContext.getProperties();
 
 		MonitoringConfiguration monitoringConfiguration =
-			Configurable.createConfigurable(
+			ConfigurableUtil.createConfigurable(
 				MonitoringConfiguration.class, properties);
 
 		DestinationConfiguration destinationConfiguration =

portal-monitoring/src/main/java/com/liferay/portal/monitoring/internal/servlet/taglib/MonitoringBottomDynamicInclude.java
@@ -14,8 +14,7 @@
 
 package com.liferay.portal.monitoring.internal.servlet.taglib;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
+import com.liferay.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.monitoring.DataSample;
 import com.liferay.portal.kernel.monitoring.DataSampleThreadLocal;
 import com.liferay.portal.kernel.monitoring.RequestStatus;
@@ -103,7 +102,7 @@ public class MonitoringBottomDynamicInclude extends BaseDynamicInclude {
 	@Activate
 	@Modified
 	protected void activate(Map<String, Object> properties) {
-		_monitoringConfiguration = Configurable.createConfigurable(
+		_monitoringConfiguration = ConfigurableUtil.createConfigurable(
 			MonitoringConfiguration.class, properties);
 	}
 

portal-monitoring/src/main/java/com/liferay/portal/monitoring/internal/servlet/taglib/MonitoringTopHeadDynamicInclude.java
@@ -14,8 +14,7 @@
 
 package com.liferay.portal.monitoring.internal.servlet.taglib;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
+import com.liferay.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.monitoring.DataSample;
 import com.liferay.portal.kernel.monitoring.DataSampleFactory;
 import com.liferay.portal.kernel.monitoring.DataSampleThreadLocal;
@@ -83,7 +82,7 @@ public class MonitoringTopHeadDynamicInclude extends BaseDynamicInclude {
 	@Activate
 	@Modified
 	protected void activate(Map<String, Object> properties) {
-		_monitoringConfiguration = Configurable.createConfigurable(
+		_monitoringConfiguration = ConfigurableUtil.createConfigurable(
 			MonitoringConfiguration.class, properties);
 	}
 

portal-upgrade/src/main/java/com/liferay/portal/upgrade/internal/release/ReleaseManager.java
@@ -14,8 +14,7 @@
 
 package com.liferay.portal.upgrade.internal.release;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
+import com.liferay.bnd.util.ConfigurableUtil;
 import com.liferay.osgi.service.tracker.collections.map.PropertyServiceReferenceComparator;
 import com.liferay.osgi.service.tracker.collections.map.PropertyServiceReferenceMapper;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
@@ -124,7 +123,7 @@ public class ReleaseManager {
 		ServiceTrackerMapListener<String, UpgradeInfo, List<UpgradeInfo>>
 			serviceTrackerMapListener = null;
 
-		_releaseManagerConfiguration = Configurable.createConfigurable(
+		_releaseManagerConfiguration = ConfigurableUtil.createConfigurable(
 			ReleaseManagerConfiguration.class, properties);
 
 		if (_releaseManagerConfiguration.autoUpgrade()) {

portal-verify-extender/src/main/java/com/liferay/portal/verify/extender/internal/VerifyProcessTracker.java
@@ -14,8 +14,7 @@
 
 package com.liferay.portal.verify.extender.internal;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
+import com.liferay.bnd.util.ConfigurableUtil;
 import com.liferay.counter.kernel.service.CounterLocalService;
 import com.liferay.osgi.service.tracker.collections.map.PropertyServiceReferenceComparator;
 import com.liferay.osgi.service.tracker.collections.map.PropertyServiceReferenceMapper;
@@ -132,8 +131,9 @@ public class VerifyProcessTracker {
 	protected void activate(
 		BundleContext bundleContext, Map<String, Object> properties) {
 
-		_verifyProcessTrackerConfiguration = Configurable.createConfigurable(
-			VerifyProcessTrackerConfiguration.class, properties);
+		_verifyProcessTrackerConfiguration =
+			ConfigurableUtil.createConfigurable(
+				VerifyProcessTrackerConfiguration.class, properties);
 
 		ServiceTrackerMapListener<String, VerifyProcess, List<VerifyProcess>>
 			verifyServiceTrackerMapListener = null;